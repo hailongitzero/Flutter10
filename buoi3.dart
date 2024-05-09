8d1158c4971939c70760ace96b4c44d681183e27
taquanghuy02032000 <taquanghuy0386356200@gmail.com>
2024-05-09 20:11:20 +0700

    done demo loop

--- buoi3.dart
+++ buoi3.dart
@@ -7,14 +7,19 @@
 void main() {
   List<int> intList = [3, 4, 1, 2, 7, 10]; // length 6
   // demoForInLoop(intList);
-  for (int index = 0; index < intList.length; index++) {
-    if (index == 2) break;
 
-    ///continue -> dùng khi không muốn lặp ở bước hiện tại <=> (điều kiện đề ra) và trở lại lặp ở
-    /// bước tiếp theo
-    ///break -> dừng lặp
-    print('number at index ${index + 1} is : ${intList[index]}');
+  List<List<int>> anArray2D = [
+    [3, 4, 5, 3],
+    [22, 11, 33],
+  ];
+  // anArray2D[row][col]; [0][0], [0][1], [0,2], [1,0,1,1,1,2]
+  for (int row = 0; row < anArray2D.length; row++) {
+    for (int col = 0; col < anArray2D[row].length; col++) {
+      print('value at [${row + 1}][${col + 1}] is ${anArray2D[row][col]}');
+    }
   }
+
+  // exercise();
   // demoDoWhileFtWhile(intList);
 }
 
@@ -63,4 +68,16 @@ void demoDoWhileFtWhile(List<int> intList) {
     print('number at index $indexCurrent is : ${intList[indexForDoWhile]}');
     indexForDoWhile++;
   } while (indexForDoWhile < intList.length);
+}
+
+///in ra tu 1 -> 100
+void exercise() {
+  int sum = 0;
+  for (int i = 1; i <= 100; i++) {
+    if (i >= 90) break;
+    if (i % 2 == 0) {
+      sum += i;
+    }
+  }
+  print(sum);
 }
