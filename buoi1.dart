994daa278202ec8cbcdb4c21a527d2ac77d1333a
taquanghuy02032000 <taquanghuy0386356200@gmail.com>
2024-05-11 20:29:13 +0700

    refactor buoi1.dart + description buoi4.dart

--- buoi1.dart
+++ buoi1.dart
@@ -1,3 +1,89 @@
+import 'dart:ffi';
+
 void main() {
-  int age = 3 + 2;
+  //int: so nguyen, String: chuoi, map: key-value, double : so thuc,  List: danh sach, dynaic
+  //final, const, late, var,
+  //how to create name for a variable : type nameVar; -> lowercase
+  int tuoi = 12;
+  print(tuoi.isOdd); //le? -> false
+
+  dynamic anotherTuoi = 12;
+  anotherTuoi as int;
+  anotherTuoi;
+
+  List<String> students = ["hoang", "huy", "long", '3'];
+
+  int x = 4;
+
+  double scoreMath = 7.4;
+
+  String name = "huy hoang";
+  String nameAnother = 'huy hoang';
+
+  Map demoMapValue = {
+    "name": "huy hoang",
+    1: "2",
+    {"name": "huy"}: 3,
+  };
+
+  Map<String, String> demoValueMapWithGenericType = {
+    ///Generictype
+    "name": "huy",
+  };
+
+  int caculatedAge = (x + 2) * 3;
+  // int caculatedAge2 = (x /4 ) + 3;
+
+  int timeGameToMinutes = 60;
+
+  bool isTimeOver = false;
+
+  bool isEnoughAge = false;
+  isEnoughAge = true;
+
+  print("hello huy hoang ${caculatedAge}");
+  print('demo ! operator ${!isEnoughAge}');
+  int a = 3;
+  a++;
+  a += 1;
+  a = a + 1; // a+=1 // a = a + 1
+  ++a;
+  a--;
+
+  dynamic age = 3;
+  List<dynamic> studentsAnother = ["hjuy", 3, (3 + 2)];
+
+  print(age);
+  age as int;
+
+  dynamic data = 30;
+  data as int;
+
+  print('dong nay ${3 is int}');
+
+  bool isInt = (3 is int); //isInt = true;
+
+  print('---');
+
+  print(age);
+
+  ////
 }
+
+// unary postfix	expr++    expr--    ()    []    ?[]    .    ?.    !	None
+// unary prefix	-expr    !expr    ~expr    ++expr    --expr      await expr	None
+// multiplicative	*    /    %  ~/	Left
+// additive	+    -	Left
+// shift	<<    >>    >>>	Left
+// bitwise AND	&	Left
+// bitwise XOR	^	Left
+// bitwise OR	|	Left
+// relational and type test	>=    >    <=    <    as    is    is!	None
+// equality	==    !=	None
+// logical AND	&&	Left
+// logical OR	||	Left
+// if-null	??	Left
+// conditional	expr1    ?    expr2    :    expr3	Right
+// cascade	..    ?..	Left
+// assignment	=    *=    /=   +=   -=   &=   ^=   etc.	Right
+// spread (See note)
--- buoi4.dart
+++ buoi4.dart
@@ -10,3 +10,69 @@
 /// 2 * 3
 /// [1,2,3]
 /// [3,4,5]
+///
+/// ==================================HIGHLIGHT==================================
+/// TÌM HIỂU THÊM,
+///typeDef
+void main() {
+  // demoSwitchCase();
+  demoTryCatch();
+}
+
+/// switch-case
+
+enum DayValue {
+  ///The weekend
+  Fri,
+  Sa,
+  Sun,
+}
+
+void demoSwitchCase() {
+  DayValue currentDay = DayValue.Fri;
+  int intValue = 2;
+  if (currentDay == DayValue.Sa) {
+    print('currentDay : ${currentDay}');
+  } else {
+    print('currentDay : ${currentDay}');
+  }
+
+  switch (intValue) {
+    case (const (8 / 4)):
+      break;
+    case (const (16 / 8)):
+      print('currentDay Sunday');
+    default:
+      print('unknown');
+  }
+}
+
+///Try-catch
+void demoTryCatch() {
+  int firstNum = 4;
+  dynamic secondNum = '2';
+  try {
+    double result = firstNum / secondNum;
+    print(result);
+    print('valueAble1');
+  } catch (dynamicException) {
+    print('null value $dynamicException');
+    print('valueAble2');
+  }
+
+  ///
+  try {
+    dynamic secNum = '3';
+    var res = 12 ~/ 0;
+    print("The result is $res");
+  } on IntegerDivisionByZeroException {
+    // throw FormatException('thiếu kìa');
+    assert(null != null);
+    print('under throw');
+  } catch (e) {
+    print("Unknown exception: $e");
+  } finally {
+    // Always clean up, even if an exception is thrown.
+    print("finally");
+  }
+}
