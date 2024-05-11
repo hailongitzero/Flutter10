dc87704ce02bff0fcec512f1bf253fe35c64b7b8
taquanghuy02032000 <taquanghuy0386356200@gmail.com>
2024-05-11 18:49:05 +0700

    add first exe coding

---
+++ buoi4.dart
@@ -0,0 +1,12 @@
+///Bài 1:
+///Viết hàm tính luỹ thừa của một số
+///Yêu cầu: Phải dùng vòng lặp
+///
+///Bài 2:
+///Viết hàm kiểm tra có phải là một tam giác hay không tham số hàm là độ dài của 3 cạnh
+///
+/// Bài 3:
+/// Cho một ma trận cỡ (n-row * m-column) (n,m thuoc N*, n&m > 0) -> in ra các số nếu là số chẵn
+/// 2 * 3
+/// [1,2,3]
+/// [3,4,5]
